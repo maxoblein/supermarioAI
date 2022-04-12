d725b36e53f6963cc8019c75801aa2c2844ce23c
Max Oblein <max.oblein@gmail.com>
2022-04-12 12:05:39 +0100

    Initial commit

---
+++ marioMain.py
@@ -0,0 +1,7 @@
+#creating environment to play super mario#
+#import game
+import gym_super_mario_bros
+#import joypad wrapper
+from nes_py.wrappers import JoypadSpace
+#import simplified controls
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
